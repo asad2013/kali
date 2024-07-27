ee2e1e8526a3b635ef63d1cb0ba508f5dcc687cf
asad2103 <asadwarsame11@gmail.com>
2024-07-27 07:38:27 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kali',
+  webDir: 'dist'
+};
+
+export default config;
